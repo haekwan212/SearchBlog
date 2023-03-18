4a8cdcad718112371f3f2583c55ba747f47a2bd8
haekwan212 <haekwan212@naver.com>
2023-03-18 19:07:06 +0900

    Add files via upload

---
+++ src/main/java/com/search/SerchBlogApplication.java
@@ -0,0 +1,13 @@
+package com.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerchBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerchBlogApplication.class, args);
+	}
+
+}
